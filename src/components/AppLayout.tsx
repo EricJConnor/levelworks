c3300e60bf14a1da6c68649f6ec12c23c1e21ef2
EricJConnor <7echome@gmail.com>
2026-04-06 18:28:06 -0400

    Update AppLayout.tsx

--- src/components/AppLayout.tsx
+++ src/components/AppLayout.tsx
@@ -173,10 +173,7 @@ export const AppLayout: React.FC = () => {
             onConnectStripe={async () => {
   const { data: { user } } = await supabase.auth.getUser();
   if (!user) return;
-  const { data } = await supabase.functions.invoke('connect-stripe-account', {
-    body: { action: 'get_oauth_url', userId: user.id }
-  });
-  if (data?.url) window.location.href = data.url;
+  window.location.href = `https://connect.stripe.com/oauth/authorize?client_id=ca_T3ss3sYTBR7iYQrEPRYmsQYyo8BI5XVA&scope=read_write&redirect_uri=https://levelworks.org/stripe-connect-callback&state=${user.id}`;
 }}
 stripeConnected={!!profile?.stripe_account_id}
             />
